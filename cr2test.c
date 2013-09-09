0674718ba426213e06255083511a4bcd443a08f4
Anton Keks <anton@codeborne.com>
2013-09-09 22:29:38 +0300

    cr2test code unified with rawfs.c

--- cr2test.c
+++ cr2test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 typedef unsigned short ushort;
 typedef unsigned int uint;
@@ -11,36 +13,38 @@ struct tiff_tag {
   union { char c[4]; short s[2]; int i; } val;
 };
 
-void find_thumb(FILE *f, int *thumb_offset, int *thumb_length) {
-	fseek(f, 16, SEEK_SET);
+void find_thumb(int fd, int *thumb_offset, int *thumb_length) {
+	lseek(fd, 16, SEEK_SET);
 	short ifd_size = 0;
-	fread(&ifd_size, 1, 2, f);
-    struct tiff_tag tag;
+	read(fd, &ifd_size, 2);
+    struct tiff_tag tags[ifd_size];
+    read(fd, tags, ifd_size * sizeof *tags);
 	for (int i = 0; i < ifd_size; i++) {
-	    fread(&tag, 1, sizeof tag, f);
-	    if (tag.id == 0x111) *thumb_offset = tag.val.i;
-	    else if (tag.id == 0x117) *thumb_length = tag.val.i;
+	    struct tiff_tag *tag = &tags[i];
+	    if (tag->id == 0x111) *thumb_offset = tag->val.i;
+	    else if (tag->id == 0x117) *thumb_length = tag->val.i;
 	}
 }
 
 int main(int argc, char* argv[]) {
-	FILE* f = fopen(argv[1], "rb");
-    if (f) {
-		int thumb_offset = 0;
-		int thumb_length = 0;
-		find_thumb(f, &thumb_offset, &thumb_length);
-		
+   	int thumb_offset = 0;
+    int thumb_length = 0;
+	int fd = open(argv[1], O_RDONLY);
+    if (fd != -1) {
+    	find_thumb(fd, &thumb_offset, &thumb_length);
 		printf("thumb_offset 0x%x, thumb_length %d\n", thumb_offset, thumb_length);
 		
-		fseek(f, thumb_offset, SEEK_SET);
+		lseek(fd, thumb_offset, SEEK_SET);
 		char *thumb = malloc(thumb_length);
-		fread(thumb, 1, thumb_length, f);
+		read(fd, thumb, thumb_length);
 		
 		char *filename = "thumb.jpg";
 		FILE* ofp = fopen(filename, "wb");
 		fwrite(thumb, 1, thumb_length, ofp);
 		fclose(ofp);
 		printf("Written %s\n", filename);
+
+		close(fd);
     }
     return 0;
 }
