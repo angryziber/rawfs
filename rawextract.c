da2c44646524fdd8e833ab48aed0ae2628110a36
Anton Keks <anton@codeborne.com>
2013-09-10 23:34:22 +0300

    write only a single file

--- rawextract.c
+++ rawextract.c
@@ -22,18 +22,11 @@ int main(int argc, char* argv[]) {
    	struct img_data img;
 	int fd = open(argv[1], O_RDONLY);
     if (fd != -1) {
-        prepare_jpeg(fd, &img);
+        prepare_jpeg(fd, &img);        
        	write_file("thumb_exif.jpg", img.out, img.out_length);
     	free(img.out);
-
-		printf("thumb_offset 0x%x, thumb_length %d, exif_offset %d\n", img.thumb_offset, img.thumb_length, img.exif_offset);
-		
-		lseek(fd, img.thumb_offset, SEEK_SET);
-		char *thumb = malloc(img.thumb_length);
-		read(fd, thumb, img.thumb_length);
-		
-		write_file("thumb.jpg", thumb, img.thumb_length);
 		close(fd);
+		printf("thumb_offset 0x%x, thumb_length %d, exif_offset %d\n", img.thumb_offset, img.thumb_length, img.exif_offset);
     }
     return 0;
 }
