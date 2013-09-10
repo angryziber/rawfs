34538e8c76b7bfac4639efbb68c7016f3260ef90
Anton Keks <anton@codeborne.com>
2013-09-11 00:02:49 +0300

    some error checking added

--- raw.c
+++ raw.c
@@ -34,38 +34,47 @@ struct img_data {
   char *out;
 };
 
-void parse_raw(int fd, struct img_data *img) {
+int parse_raw(int fd, struct img_data *img) {
     memset(img, 0, sizeof *img);
+    int res = 0;
     
 	lseek(fd, CR2_HEADER_LENGTH, SEEK_SET);
-	read(fd, &img->ifd_size, sizeof img->ifd_size);
+	res = read(fd, &img->ifd_size, sizeof img->ifd_size);
+	if (res < 0) return res;
+	
     struct tiff_tag tags[img->ifd_size];
-    read(fd, tags, img->ifd_size * sizeof *tags);
+    res = read(fd, tags, img->ifd_size * sizeof *tags);
+  	if (res < 0) return res;
+
 	for (int i = 0; i < img->ifd_size; i++) {
 	    struct tiff_tag *tag = &tags[i];
 	    if (tag->id == 0x111) img->thumb_offset = tag->val.i;
 	    else if (tag->id == 0x117) img->thumb_length = tag->val.i;
 	}
-	read(fd, &img->ifd2_offset, 4);
 
-	img->out_length = EXIF_HEADER_LENGTH + img->thumb_length-2 + img->ifd2_offset;		
+	res = read(fd, &img->ifd2_offset, 4);
+	img->out_length = EXIF_HEADER_LENGTH + img->thumb_length-2 + img->ifd2_offset;
+	return res;
 }
 
-void prepare_jpeg(int fd, struct img_data *img) {
-    parse_raw(fd, img);
+int prepare_jpeg(int fd, struct img_data *img) {
+    int res = 0;
+    res = parse_raw(fd, img);
+   	if (res < 0) return res;
+
 	char *outp = img->out = malloc(img->out_length);
 	
 	memcpy(outp, EXIF_HEADER, EXIF_HEADER_LENGTH);
 	*(short*)&outp[4] = htons(8 + img->ifd2_offset);  // exif data size in the APP1 marker
 	outp += EXIF_HEADER_LENGTH;
 
-	lseek(fd, 0, SEEK_SET);
-	read(fd, outp, img->ifd2_offset); // tiff header + exif data + etc
+	res = pread(fd, outp, img->ifd2_offset, 0); // tiff header + exif data + etc
+	if (res < 0) return res;
+
 	int ifd_length = sizeof img->ifd_size + img->ifd_size * sizeof(struct tiff_tag);
 	*(int*)&outp[CR2_HEADER_LENGTH + ifd_length] = 0;
 	outp += img->ifd2_offset;
 		
-	lseek(fd, img->thumb_offset+2, SEEK_SET); // skip 2 bytes - jpeg marker that is already included in EXIF_HEADER
-	read(fd, outp, img->thumb_length-2);
+	return pread(fd, outp, img->thumb_length-2, img->thumb_offset+2); // skip 2 bytes - jpeg marker that is already included in EXIF_HEADER
 }
 
