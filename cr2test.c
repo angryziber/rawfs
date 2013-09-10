35c829e9c4fe09150521a7156becc6ac3510b6c3
Anton Keks <anton@codeborne.com>
2013-09-10 23:04:45 +0300

    extracted function

--- cr2test.c
+++ cr2test.c
@@ -26,6 +26,7 @@ struct tiff_tag {
 };
 
 struct img_data {
+  short ifd_size;
   int thumb_offset;
   int thumb_length;
   int exif_offset;
@@ -42,21 +43,22 @@ void find_thumb(int fd, struct img_data *img) {
     memset(img, 0, sizeof *img);
     
 	lseek(fd, CR2_HEADER_LENGTH, SEEK_SET);
-	short ifd_size = 0;
-	read(fd, &ifd_size, sizeof ifd_size);
-    struct tiff_tag tags[ifd_size];
-    read(fd, tags, ifd_size * sizeof *tags);
-	for (int i = 0; i < ifd_size; i++) {
+	read(fd, &img->ifd_size, sizeof img->ifd_size);
+    struct tiff_tag tags[img->ifd_size];
+    read(fd, tags, img->ifd_size * sizeof *tags);
+	for (int i = 0; i < img->ifd_size; i++) {
 	    struct tiff_tag *tag = &tags[i];
 	    if (tag->id == 0x111) img->thumb_offset = tag->val.i;
 	    else if (tag->id == 0x117) img->thumb_length = tag->val.i;
 	    else if (tag->id == 0x8769) img->exif_offset = tag->val.i;
 	}
-	
 	read(fd, &img->ifd2_offset, 4);
-	printf("ifd2_offset %d\n", img->ifd2_offset);
 
 	img->out_length = EXIF_HEADER_LENGTH + img->thumb_length-2 + img->ifd2_offset;		
+}
+
+void write_jpeg(int fd, const char *filename, struct img_data *img) {
+    find_thumb(fd, img);
 	char *outp = img->out = malloc(img->out_length);
 	
 	memcpy(outp, EXIF_HEADER, EXIF_HEADER_LENGTH);
@@ -65,13 +67,14 @@ void find_thumb(int fd, struct img_data *img) {
 
 	lseek(fd, 0, SEEK_SET);
 	read(fd, outp, img->ifd2_offset); // tiff header + exif data + etc
-	*(int*)&outp[CR2_HEADER_LENGTH + sizeof ifd_size + ifd_size * sizeof *tags] = 0;
+	int ifd_leng    th = sizeof img->ifd_size + img->ifd_size * sizeof(struct tiff_tag);
+	*(int*)&outp[CR2_HEADER_LENGTH + ifd_length] = 0;
 	outp += img->ifd2_offset;
 		
 	lseek(fd, img->thumb_offset+2, SEEK_SET); // skip 2 bytes - jpeg marker that is already included in EXIF_HEADER
 	read(fd, outp, img->thumb_length-2);
 	
-	write_file("thumb_exif.jpg", img->out, img->out_length);
+	write_file(filename, img->out, img->out_length);
 	free(img->out);
 }
 
@@ -79,7 +82,7 @@ int main(int argc, char* argv[]) {
    	struct img_data img;
 	int fd = open(argv[1], O_RDONLY);
     if (fd != -1) {
-    	find_thumb(fd, &img);
+        write_jpeg(fd, "thumb_exif.jpg", &img);
 		printf("thumb_offset 0x%x, thumb_length %d, exif_offset %d\n", img.thumb_offset, img.thumb_length, img.exif_offset);
 		
 		lseek(fd, img.thumb_offset, SEEK_SET);
