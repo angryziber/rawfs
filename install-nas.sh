0de2a0c95a55b15f58bfda3f70dbbe3c419ffb89
Anton Keks <anton@codeborne.com>
2015-07-22 00:40:34 +0300

    will install files to /opt, hopefully it will persist between synology software upgrades

--- install-nas.sh
+++ install-nas.sh
@@ -9,6 +9,10 @@ fi
 NAS=nas.local
 PHOTOS=/volume1/Photos
 MOUNTPOINT=/volume1/photo
+PREFIX=/opt
+START_SCRIPT=$PREFIX/etc/init.d/S99rawfs
 
-scp libfuse.so.2 rawfs-arm $NAS:
-ssh $NAS "LD_LIBRARY_PATH=. ./rawfs-arm $PHOTOS $MOUNTPOINT -o allow_other"
+scp libfuse.so.2 rawfs-arm $NAS:$PREFIX/bin
+ssh $NAS "echo 'LD_LIBRARY_PATH=$PREFIX/bin $PREFIX/bin/rawfs-arm $PHOTOS $MOUNTPOINT -o allow_other' > $START_SCRIPT;
+chmod a+x $START_SCRIPT;
+$START_SCRIPT"
