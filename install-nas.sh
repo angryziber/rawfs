53fa7c70d756e4f220bd0bc4909cac511da97f8c
Anton Keks <anton@azib.net>
2018-03-04 15:50:09 +0200

    store files in /volume1 - /root seems to be cleared on some DSM updates

--- install-nas.sh
+++ install-nas.sh
@@ -9,11 +9,11 @@ fi
 NAS=nas.local
 PHOTOS=/volume1/Photos
 MOUNTPOINT=/volume1/photo
-DEST=/root
-START_SCRIPT=/etc/rc.rawfs
+DEST=/volume1
+START_SCRIPT=/volume1/rawfs.sh
 
 scp rawfs-arm $NAS:$DEST
 ssh $NAS "echo 'rm -fr $MOUNTPOINT/@eaDir; $DEST/rawfs-arm $PHOTOS $MOUNTPOINT -o allow_other' > $START_SCRIPT;
 chmod a+x $START_SCRIPT;
 $START_SCRIPT"
-echo "You may add a triggered task now /etc/rc.rawfs to start rawfs after reboot"
+echo "You may add a task in task scheduler using $START_SCRIPT to start rawfs after reboot"
