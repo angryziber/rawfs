99027369867f24e3901141f4cf08a7bb65e4b5b7
Anton Keks <anton@codeborne.com>
2021-04-25 20:06:31 +0300

    substituting symbols doesn't work as old __lxstat64 has a different signature, building in docker is easier for Synology

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-GCC_OPTS="-g -rdynamic -Wall -std=gnu99 `pkg-config fuse --cflags --libs`"
+#!/bin/bash
+# To compile with GLIBC < 2.33 (for Synology, which uses GLIBC 2.20), run in:
+# docker run -ti -v `pwd`:/app ubuntu:20.04 'apt update && apt install gcc pkg-config libfuse-dev && bash'
+GCC_OPTS="-g -rdynamic -Wall -std=gnu99 -D_FILE_OFFSET_BITS=64 `pkg-config fuse --cflags --libs`"
 gcc rawextract.c $GCC_OPTS -o rawextract &&
 gcc rawfs.c $GCC_OPTS  -o rawfs
